bb43f5afa9b835575229d1257657027a205fb326
chuanzong.lcz@alibaba-inc.com <chuanzong.lcz@alibaba-inc.com>
2019-11-03 21:32:02 +0800

    refactor(client): if there is no token in url, no need to append it to the api

--- client/src/utils/fetch.js
+++ client/src/utils/fetch.js
@@ -1,7 +1,7 @@
 import token from './token';
 
 export default async function fetch(api) {
-  const resp = await window.fetch(`${api}?token=${token}`);
+  const resp = await window.fetch(!token ? api : `${api}?token=${token}`);
 
   if (resp.status !== 200) {
     throw new Error(resp.status + ' ' + resp.statusText);
